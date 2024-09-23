28f9fe2adcd68fff6bc377480f3a6aee23fc6595
krish <thinkperfect25@gmail.com>
2024-09-22 20:28:13 -0700

    horizontal slide code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
@@ -3,9 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.SimpleExamples.HorizExten;
 
 @Config
 @TeleOp(name = "Driver Control 2025", group = "TeleOp")
@@ -21,6 +29,7 @@ public class DriveControl extends  OpMode {
     //private static final Logger log = LoggerFactory.getLogger(DriveControl.class);
     private static final double EXTENDEDPOS = 0.5;
     private static final double RETRACTEDPOS = 0;
+
     private static int LOWPOS = 0;
     private static int MEDIUMPOS = 300;
     private static int HIGHPOS = 600;
@@ -45,9 +54,13 @@ public class DriveControl extends  OpMode {
     Boolean lb2Last = false;
     Boolean lb2Toggle = false;
 
-    Boolean y2Current = false;
-    Boolean y2Last = false;
-    Boolean y2Toggle = false;
+    //Boolean rb2Current = false;
+    //Boolean rb2Last = false;
+    //Boolean rb2Toggle = false;
+
+    Boolean c2Current = false;
+    Boolean c2Last = false;
+    Boolean c2Toggle = false;
 
 
     @Override
@@ -78,7 +91,6 @@ public class DriveControl extends  OpMode {
         x2Last = x2Current;
 
         a2Current = gamepad2.a;
-
             if (a2Current && !a2Last) {
                 a2Toggle = !a2Toggle;
             }
@@ -104,20 +116,26 @@ public class DriveControl extends  OpMode {
             }
 
         b2Last = b2Current;
+            c2Current = gamepad2.y;
+                if (c2Current && !c2Last) {
+                    c2Toggle = !c2Toggle;
+                }
+                if (b2Toggle){
+                    hw.LHoriz.setPosition(EXTENDEDPOS);
+                    hw.RHoriz.setPosition(EXTENDEDPOS);
+                }else{
+                    hw.LHoriz.setPosition(RETRACTEDPOS);
+                    hw.RHoriz.setPosition(RETRACTEDPOS);
+                }
+         c2Last = c2Current;
+             if (gamepad2.dpad_down)  {
+                 target = LOWPOS;
+             }else if (gamepad2.dpad_left || gamepad2.dpad_right) {
+                 target = MEDIUMPOS;
+             }else if (gamepad2.dpad_up){
+                 target = HIGHPOS;
+             }
 
-        y2Current = gamepad2.y;
-
-            if (y2Current && !y2Last) {
-                y2Toggle = !y2Toggle;
-            }
-            if (y2Toggle){
-                hw.HorExt.HExtend();
-            }
-            else{
-                hw.HorExt.HRetract();
-            }
-
-        y2Last = y2Current;
 
         lb2Current = gamepad2.left_bumper;
 
@@ -132,14 +150,6 @@ public class DriveControl extends  OpMode {
             }
 
         lb2Last = lb2Current;
-
-        if (gamepad2.dpad_down){
-            target = LOWPOS;
-        }else if (gamepad2.dpad_left || gamepad2.dpad_right){
-            target = MEDIUMPOS;
-        } else if (gamepad2.dpad_up){
-            target = HIGHPOS;
-        }
 
         double Drive = -gamepad1.left_stick_y;
         double Turn = gamepad1.right_stick_x;
